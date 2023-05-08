ff9d3f9df95264f1e529ed317a82725c846c381d
prisci <priscilaocolaotmail.com>
2023-05-08 10:31:19 -0300

    commit

--- scripts/upcomingEvents.js
+++ scripts/upcomingEvents.js
@@ -3,6 +3,7 @@ let upcoming = document.getElementById("upcoming-cards");
 
 function planoCards(objeto) {
   return `
+      <div class="card mt-3 mb-3" style="width: 18rem;">
       <img src="${objeto.image}" class="card-img-top object-fit-cover w-100" alt="cine img">
       <div class="card-body text-center">
       <h5 class="card-title">${objeto.name}</h5>
@@ -11,6 +12,7 @@ function planoCards(objeto) {
       <div class="card-footer d-flex justify-content-between align-items-center">
       <h6 class="m-0">$${objeto.price}</h6>
       <a href="../pages/details.html" class="btn btn-primary">Details</a>
+      </div>
       </div>
   `;
 }
